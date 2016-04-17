4bf60a800c34e0b41e20daa2c7a626937fe4f6b8
Dylan Harness <dharness@uwo.ca>
2016-04-17 14:11:37 -0400

    ..

--- shipitfile.js
+++ shipitfile.js
@@ -1,7 +1,8 @@
 module.exports = function (shipit) {
   shipit.initConfig({
     staging: {
-      servers: 'dharness@107.170.2.166'
+      servers: 'dharness@107.170.2.166',
+      key: './rsa.pub',
     }
   });
 
