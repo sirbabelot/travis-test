21789d42d2f26e649cb726a3a81fca9df7c6955c
naila <23naila.n@gmail.com>
2016-04-22 14:39:32 -0400

    playing with noodle

---
+++ shipitfile.js
@@ -0,0 +1,21 @@
+const DOCKER_REMOVE = 'docker rm -f $(docker ps -aq)'
+const DOCKER_PULL = 'docker pull bablot/travis-test';
+const DOCKER_RUN = 'docker run -d --name bablotcontainer -p 8888:8888 bablot/travis-test npm start';
+
+module.exports = function(shipit) {
+  shipit.initConfig({
+    staging: {
+      servers: 'nailanur@107.170.2.166'
+    }
+  });
+
+  shipit.task('deploy', ()=> {
+    return shipit
+        .remote(`
+          if ! [ -z $(docker ps -aq -f name=bablotcontainer) ]; 
+            then docker rm -f bablotcontainer
+          fi`)
+        .then(()=> shipit.remote(DOCKER_PULL))
+        .then(()=> shipit.remote(DOCKER_RUN));
+  });
+};
