fa538a3d596f4abe50e6bc522ca031e5aa4096ed
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2016-05-06 15:01:24 -0400

    good guy wes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f morgtest
 docker pull bablot/travis-test
-docker run -p 80:8888 --name morgtest bablot/travis-test
+docker run -d -p 80:8888 --name morgtest bablot/travis-test
