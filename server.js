6c3abfc361a73ced738574adf304bd9329691e1f
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2016-05-06 14:59:40 -0400

    good guy wes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 
 app.get('/', function(req, res) {
-  res.send('HELLO MY DEPLOYMENT GUIDE TO DEPLOYMENT AND MARGAN!');
+  res.send('HELLO MY DEPLOYMENT GUIDE TO DEPLOYMENT AND WESWESSELFIG!');
 });
 
 app.listen(8888, function() {
