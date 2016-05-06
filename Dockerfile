a28d45f44aaf36f7df8432440a04ba7988ca3962
Dylan Harness <dharness@uwo.ca>
2016-05-06 10:41:46 -0400

    removed node_modules and all master code

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:5.11.0
-
-RUN mkdir /usr/src/app
-
-WORKDIR /usr/src/app
-
-COPY ./ /usr/src/app
-
-EXPOSE 8888
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-if ! [ -z $(docker ps -aq -f name=bablotcontainer) ];
-  then docker rm -f bablotcontainer
-  else echo "No containers to delete!"
-fi
